ded3a11547bc5f5a2d79940f95fd0016701da885
PieroCaramutt <caramutt@sheridancollege.ca>
2023-05-30 19:56:30 -0400

    new commit

---
+++ src/mathwithjava/User.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mathwithjava;
+
+/**
+ *
+ * @author Piero
+ */
+public class User {
+    
+    private String userName;
+    private String email;
+    
+    public User(String uName, String uEmail) {
+        userName = uName;
+        email = uEmail;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
